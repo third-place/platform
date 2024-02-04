7cc3f7340ea1c14786fc863a084ddcb710da0cf4
Dan Munro <dan@danmunro.com>
2024-02-03 19:24:23 -0800

    setup argocd and system in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 set -e
 
-#helm repo add argocd https://argoproj.github.io/argo-helm
-#helm dependency update argocd
-#helm upgrade --install --namespace argocd --create-namespace -f argocd/values.yaml argocd ./argocd
+helm repo add argocd https://argoproj.github.io/argo-helm
+helm dependency update argocd
+helm upgrade --install --namespace argocd --create-namespace -f argocd/values.yaml argocd ./argocd
 
-helm upgrade --install --namespace registry --create-namespace -f registry/values.yaml registry ./registry
+helm upgrade --install --namespace system --create-namespace -f system/values.yaml system ./system
