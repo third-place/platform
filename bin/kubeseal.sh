bda7cb68641a2c1100bf57a4c5cb561d4beb82ef
Dan Munro <dan@danmunro.com>
2024-02-05 19:35:46 -0800

    kubeseal script

---
+++ bin/kubeseal.sh
@@ -0,0 +1,9 @@
+set -e
+
+NAMESPACE=$1
+SECRET_NAME=$2
+SECRET_VALUE=$3
+
+kubeseal --fetch-cert --controller-namespace sealed-secrets --controller-name sealed-secrets > public-key-cert.pem
+echo -n $SECRET_VALUE | kubeseal --controller-namespace sealed-secrets --controller-name sealed-secrets --cert=public-key-cert.pem --raw --name $SECRET_NAME --namespace $NAMESPACE --scope namespace-wide
+rm public-key-cert.pem
