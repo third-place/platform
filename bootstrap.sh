d6c5d851e75eaa1b7a45806542a4dccd2821f91a
Dan Munro <dan@danmunro.com>
2024-02-12 17:59:10 -0600

    latest changes to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 set -e
 
+k3d cluster create -p "9000:80@loadbalancer" --agents 2
+
 helm repo add argocd https://argoproj.github.io/argo-helm
 helm dependency update argocd
-helm upgrade --install --namespace argocd --create-namespace -f argocd/values.yaml argocd ./argocd
 
+helm upgrade --install --namespace argocd --create-namespace -f argocd/values.yaml argocd ./argocd
 helm upgrade --install --namespace system --create-namespace -f system/values.yaml system ./system
