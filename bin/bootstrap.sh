1051d7faf4470cc4f75e9b00c8791a515be9254b
Dan Munro <dan@danmunro.com>
2024-02-18 08:04:28 -0800

    helm commands for each environment

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,4 +7,7 @@ helm repo add argocd https://argoproj.github.io/argo-helm
 helm dependency update argocd
 
 helm upgrade --install --namespace argocd --create-namespace -f argocd/values.yaml argocd ./argocd
-helm upgrade --install --namespace system --create-namespace -f system/values.yaml system ./system
+
+# helm upgrade --install --namespace system --create-namespace -f system/values.yaml --set global.env=dev system ./system
+# helm upgrade --install --namespace system --create-namespace -f system/values.yaml --set global.env=staging system ./system
+# helm upgrade --install --namespace system --create-namespace -f system/values.yaml --set global.env=prod system ./system
